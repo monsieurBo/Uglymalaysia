7047192e1dea16dc62716c78cc846302f3f24def
benjibennn <benjaminwong94@gmail.com>
2018-06-30 01:20:36 +0800

    taken out if empty at index

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'http://uglymalaysia.vz2k7vdg9a.ap-southeast-1.elasticbeanstalk.com', port: 3306 }
+  config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL"],
+  password: ENV["GMAIL_PASSWORD"]
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
