a1bbf6694553f89cad14576838547b9baa84613e
monsieurBo <boscoho.hy@gmail.com>
2018-07-02 13:11:36 +0800

    fix navbar bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',registrations: "users/registrations" }
 
-  get 'articles/most_views', to: 'articles#most_views'
-  get 'articles/trending', to: 'articles#trending'
+  get 'durian_runtuh', to: 'articles#most_views', as: :durian_runtuh
+  get 'pedas', to: 'articles#trending', as: :pedas
   get 'articles/search/:tag', to: 'articles#tag_search', as: :tag
   root 'articles#index'
   resources :articles do
